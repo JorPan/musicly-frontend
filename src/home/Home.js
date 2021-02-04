3d53d67ed481c73838c14babca8f1802bca308df
JorPan <jbpanasewicz@gmail.com>
2021-02-03 17:11:17 -0700

    fixed certain hover aspects and routes planning

--- src/home/Home.js
+++ src/home/Home.js
@@ -9,47 +9,52 @@ import Setly from "./setly.png";
 
 export default function Header() {
   return (
-    <div className="about">
-      <div>
-        <Link to="/modeify">
-          <img
-            className="modeify-logo logo-card"
-            alt="modeify-logo"
-            src={Modeify}
-          ></img>
-        </Link>
-        <Link to="/lyricize">
-          <img
-            className="lyricize-logo logo-card"
-            alt="lyricize-logo"
-            src={Lyricize}
-          ></img>
-        </Link>
+    <div className="home">
+      <div className="about">
+        <div>
+          <Link to="/modeify">
+            <img
+              className="modeify-logo logo-card"
+              alt="modeify-logo"
+              src={Modeify}
+            ></img>
+          </Link>
+          <Link to="/lyricize">
+            <img
+              className="lyricize-logo logo-card"
+              alt="lyricize-logo"
+              src={Lyricize}
+            ></img>
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <img
+              className="home-logo logo-card"
+              alt="main-logo"
+              src={MainLogo}
+            ></img>
+          </Link>
+        </div>
+        <div>
+          <Link to="/riffly">
+            <img
+              className="riffly-logo logo-card"
+              alt="riffly-logo"
+              src={Riffly}
+            ></img>
+          </Link>
+          <Link to="/setly">
+            <img
+              className="setly-logo logo-card"
+              alt="setly-logo"
+              src={Setly}
+            ></img>
+          </Link>
+        </div>
       </div>
-      <div>
-        <Link to="/">
-          <img
-            className="home-logo logo-card"
-            alt="main-logo"
-            src={MainLogo}
-          ></img>
-        </Link>
-      </div>
-      <div>
-        <Link to="/riffly">
-          <img
-            className="riffly-logo logo-card"
-            alt="riffly-logo"
-            src={Riffly}
-          ></img>
-        </Link>
-        <Link to="/setly">
-          <img
-            className="setly-logo logo-card"
-            alt="setly-logo"
-            src={Setly}
-          ></img>
-        </Link>
+      <div className="description">
+        {/* <Route path="/about" component={MusiclyDescription} /> */}
       </div>
     </div>
   );
