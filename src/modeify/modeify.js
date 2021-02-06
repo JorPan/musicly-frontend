ec621449247b5d8713fa992a86636516cb606752
JorPan <jbpanasewicz@gmail.com>
2021-02-05 17:15:37 -0700

    hooks piano working, need to work on visual feedback for multi-notes, and chord builder

--- src/modeify/modeify.js
+++ src/modeify/modeify.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import Modeify from "../home/modeify.png";
+import Piano from "./PIANO/Piano";
+import "./PIANO/Keyboard.css";
+import { Button } from "@material-ui/core";
 
 export default class MainMode extends Component {
+  state = {
+    mode: "",
+    notesPlaying: [],
+    chordBuilder: [],
+    playing: false,
+  };
+
   render() {
     return (
-      <div>
+      <div className="modeify-main">
+        <div className="piano">
+          <Piano className="piano" />
+        </div>
+        <div className="under-keys">
+          <div>
+            {/* <Button className="mode-button" variant="outlined">
+              {" "}
+              Change Mode{" "}
+            </Button> */}
+          </div>
+        </div>
+
         <img
           className="modeify-page-logo"
           alt="modeify-logo"
