eb1ec34f4194c6addfbd5fe03023c1ba0b57eff4
JorPan <jbpanasewicz@gmail.com>
2021-02-08 20:55:28 -0700

    refactored to hooks for modeify

--- src/modeify/modeify.js
+++ src/modeify/modeify.js
@@ -1,212 +1,143 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Modeify from "../home/modeify.png";
 import Piano from "./PIANO/Piano";
-import Keyboard from "./Keyboard";
+// import Keyboard from "./Keyboard";
 import "./PIANO/Keyboard.css";
 import "./Modeify.css";
 import { Button } from "@material-ui/core";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import KeyboardMap from "./PIANO/utils/KeyboardMap";
+import options from "./PIANO/utils/Options";
 
-export default class MainMode extends Component {
-  state = {
+const MainMode = () => {
+  const [state, setState] = useState({
     piano: "hooks",
     mode: "play",
     notesPlaying: [],
     chordBuilder: [],
     sound: "acoustic_grand_piano",
+    newNotes: false,
+  });
+
+  const isRegularKey = (event) => {
+    return !event.ctrlKey && !event.metaKey && !event.shiftKey;
   };
 
-  changePiano = () => {
-    if (this.state.piano === "hooks") {
-      this.setState({ piano: "react" });
-    } else {
-      this.setState({ piano: "hooks" });
+  // const isShiftKey = (event) => {
+  //   if (event.shiftKey) {
+  //     console.log("shift");
+  //     const chord = [...state.notesPlaying];
+  //     setState({ ...state, chordBuilder: chord });
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
+
+  // const changePiano = () => {
+  //   if (state.piano === "hooks") {
+  //     setState({ ...state, piano: "react" });
+  //   } else {
+  //     setState({ ...state, piano: "hooks" });
+  //   }
+  // };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [state.notesPlaying]);
+
+  const getNoteFromKeyboardKey = (keyboardKey) => {
+    return KeyboardMap[keyboardKey.toUpperCase()];
+  };
+
+  const handleKeyDown = (e) => {
+    if (isRegularKey(e) && !e.repeat) {
+      const note = getNoteFromKeyboardKey(e.key);
+      if (note && !state.notesPlaying.includes(note)) {
+        setState({
+          ...state,
+          newNotes: true,
+          notesPlaying: [...state.notesPlaying, note],
+        });
+      }
+    }
+    // else if (isShiftKey(e) && !e.repeat) {
+    //   let chord = [...state.notesPlaying];
+    //   setState({ ...state, chordBuilder: chord });
+    // }
+  };
+
+  const handleKeyUp = (e) => {
+    if (isRegularKey(e) && !e.repeat) {
+      const note = getNoteFromKeyboardKey(e.key);
+      if (note) {
+        setState({
+          ...state,
+          newNotes: false,
+          notesPlaying: state.notesPlaying.filter(
+            (notePlaying) => notePlaying !== note
+          ),
+        });
+      }
     }
   };
 
-  options = [
-    "acoustic_grand_piano",
-    "accordion",
-    "acoustic_bass",
-    "acoustic_guitar_nylon",
-    "acoustic_guitar_steel",
-    "agogo",
-    "alto_sax",
-    "applause",
-    "bagpipe",
-    "banjo",
-    "baritone_sax",
-    "bassoon",
-    "bird_tweet",
-    "blown_bottle",
-    "brass_section",
-    "breath_noise",
-    "bright_acoustic_piano",
-    "celesta",
-    "cello",
-    "choir_aahs",
-    "church_organ",
-    "clarinet",
-    "clavinet",
-    "contrabass",
-    "distortion_guitar",
-    "drawbar_organ",
-    "dulcimer",
-    "electric_bass_finger",
-    "electric_bass_pick",
-    "electric_grand_piano",
-    "electric_guitar_clean",
-    "electric_guitar_jazz",
-    "electric_guitar_muted",
-    "electric_piano_1",
-    "electric_piano_2",
-    "english_horn",
-    "fiddle",
-    "flute",
-    "french_horn",
-    "fretless_bass",
-    "fx_1_rain",
-    "fx_2_soundtrack",
-    "fx_3_crystal",
-    "fx_4_atmosphere",
-    "fx_5_brightness",
-    "fx_6_goblins",
-    "fx_7_echoes",
-    "fx_8_scifi",
-    "glockenspiel",
-    "guitar_fret_noise",
-    "guitar_harmonics",
-    "gunshot",
-    "harmonica",
-    "harpsichord",
-    "helicopter",
-    "honkytonk_piano",
-    "kalimba",
-    "koto",
-    "lead_1_square",
-    "lead_2_sawtooth",
-    "lead_3_calliope",
-    "lead_4_chiff",
-    "lead_5_charang",
-    "lead_6_voice",
-    "lead_7_fifths",
-    "lead_8_bass__lead",
-    "marimba",
-    "melodic_tom",
-    "music_box",
-    "muted_trumpet",
-    "oboe",
-    "ocarina",
-    "orchestra_hit",
-    "orchestral_harp",
-    "overdriven_guitar",
-    "pad_1_new_age",
-    "pad_2_warm",
-    "pad_3_polysynth",
-    "pad_4_choir",
-    "pad_5_bowed",
-    "pad_6_metallic",
-    "pad_7_halo",
-    "pad_8_sweep",
-    "pan_flute",
-    "percussive_organ",
-    "percussion",
-    "piccolo",
-    "pizzicato_strings",
-    "recorder",
-    "reed_organ",
-    "reverse_cymbal",
-    "rock_organ",
-    "seashore",
-    "shakuhachi",
-    "shamisen",
-    "shanai",
-    "sitar",
-    "slap_bass_1",
-    "slap_bass_2",
-    "soprano_sax",
-    "steel_drums",
-    "string_ensemble_1",
-    "string_ensemble_2",
-    "synth_bass_1",
-    "synth_bass_2",
-    "synth_brass_1",
-    "synth_brass_2",
-    "synth_choir",
-    "synth_drum",
-    "synth_strings_1",
-    "synth_strings_2",
-    "taiko_drum",
-    "tango_accordion",
-    "telephone_ring",
-    "tenor_sax",
-    "timpani",
-    "tinkle_bell",
-    "tremolo_strings",
-    "trombone",
-    "trumpet",
-    "tuba",
-    "tubular_bells",
-    "vibraphone",
-    "viola",
-    "violin",
-    "voice_oohs",
-    "whistle",
-    "woodblock",
-    "xylophone",
-  ];
+  let defaultOption = options[0];
 
-  defaultOption = this.options[0];
-
-  onSelect = (event) => {
-    this.setState({ sound: event.value });
+  const onSelect = (event) => {
+    setState({ ...state, sound: event.value });
   };
 
-  render() {
-    return (
-      <div className="modeify-main">
-        <div className="piano">
-          {this.state.piano === "hooks" ? (
-            <Piano className="piano" sound={this.state.sound} />
-          ) : (
-            <Keyboard keyboardMap={KeyboardMap} />
-          )}
+  return (
+    <div className="modeify-main">
+      <div className="piano">
+        <Piano
+          newNotes={state.newNotes}
+          notesPlaying={state.notesPlaying}
+          className="piano"
+          sound={state.sound}
+        />
+      </div>
+      {state.piano === "hooks" ? (
+        <div>
+          <Dropdown
+            className="sound-dropdown"
+            options={options}
+            onChange={onSelect}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
         </div>
-        {this.state.piano === "hooks" ? (
+      ) : null}
+
+      <div className="under-keys">
+        <img
+          className="modeify-page-logo"
+          alt="modeify-logo"
+          src={Modeify}
+        ></img>
+        <div className="mode-button-div">
+          <div className="space"></div>
+
           <div>
-            <Dropdown
-              className="sound-dropdown"
-              options={this.options}
-              onChange={this.onSelect}
-              value={this.defaultOption}
-              placeholder="Select an option"
-            />
-          </div>
-        ) : null}
-
-        <div className="under-keys">
-          <img
-            className="modeify-page-logo"
-            alt="modeify-logo"
-            src={Modeify}
-          ></img>
-          <div className="mode-button-div">
-            <div className="space"></div>
-
-            <div>
-              <Button
-                onClick={this.changePiano}
-                className="mode-button"
-                variant="outlined"
-              >
-                Change Piano Mode
-              </Button>
-            </div>
+            {/* <Button
+              onClick={changePiano}
+              className="mode-button"
+              variant="outlined"
+            >
+              Change Piano Mode
+            </Button> */}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default MainMode;
